85b0aa53872b030c6905934824aab4c3e0c071de
Denis <d.saushkin@directline.digital>
2022-03-05 13:46:05 +0400

    fx choice picker

--- choice_picker.py
+++ choice_picker.py
@@ -12,4 +12,11 @@ def make_a_choice(population, weights):
         weights=weights,
         k=1,
     )
-    return result
+    return result[0] if len(result) else None
+
+
+if __name__ == "__main__":
+    print(make_a_choice(
+        population=[EMOJI, PHOTO],
+        weights=[0.8, 0.2]
+    ))
